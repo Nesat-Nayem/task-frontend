de149a3f01b708b06d6a42d7edb0989812187040
Nesat Nayem <nesatnayem042@gmail.com>
2024-03-13 17:20:22 +0600

    main task is done

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Bar } from "react-chartjs-2";
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+import ChartDataLabels from "chartjs-plugin-datalabels";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -68,7 +68,7 @@ export default function Home() {
   };
 
   const ageGenderChartOptions = {
-    indexAxis: 'y',
+    indexAxis: "y",
     responsive: true,
     plugins: {
       tooltip: {
@@ -102,201 +102,209 @@ export default function Home() {
       },
     },
   };
-  
 
   // Calculate the total count for all locations to display percentages
-  const totalLocationCount = data.locationData.reduce((sum, item) => sum + item.count, 0);
-// Prepare the data for the location chart
-const locationLabels = data.locationData.map((item) => item._id);
-const locationData = data.locationData.map((item) => item.count);
-const locationTotal = locationData.reduce((sum, count) => sum + count, 0);
+  const totalLocationCount = data.locationData.reduce(
+    (sum, item) => sum + item.count,
+    0
+  );
+  // Prepare the data for the location chart
+  const locationLabels = data.locationData.map((item) => item._id);
+  const locationData = data.locationData.map((item) => item.count);
+  const locationTotal = locationData.reduce((sum, count) => sum + count, 0);
 
-const locationChartData = {
-  labels: locationLabels,
-  datasets: [
-    {
-      label: 'Amount',
-      data: locationData,
-      backgroundColor: '#44f1b6',
-      datalabels: {
-        color: 'black',
-        anchor: 'end',
-        align: 'end',
-        formatter: (value, context) => {
-          const percentage = ((value / locationTotal) * 100).toFixed(1);
-          return `${percentage}%`;
+  const locationChartData = {
+    labels: locationLabels,
+    datasets: [
+      {
+        label: "Amount",
+        data: locationData,
+        backgroundColor: "#44f1b6",
+        datalabels: {
+          color: "black",
+          anchor: "end",
+          align: "end",
+          formatter: (value, context) => {
+            const percentage = ((value / locationTotal) * 100).toFixed(1);
+            return `${percentage}%`;
+          },
+        },
+      },
+    ],
+  };
+
+  const locationChartOptions = {
+    indexAxis: "y",
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          callback: function (value) {
+            return this.getLabelForValue(value);
+          },
+        },
+      },
+      x: {
+        ticks: {
+          display: false,
         },
       },
     },
-  ],
-};
-
-const locationChartOptions = {
-  indexAxis: 'y',
-  responsive: true,
-  scales: {
-    y: {
-      ticks: {
-        callback: function (value) {
-          return this.getLabelForValue(value);
+    plugins: {
+      tooltip: {
+        callbacks: {
+          title: (context) => context[0].label,
+          label: (context) => {
+            const value = context.parsed.x;
+            const percentage = ((value / locationTotal) * 100).toFixed(1);
+            return `Amount: ${value} (${percentage}%)`;
+          },
+          footer: () => `Total: ${locationTotal}`,
         },
       },
-    },
-    x: {
-      ticks: {
+      legend: {
         display: false,
       },
-    },
-  },
-  plugins: {
-    tooltip: {
-      callbacks: {
-        title: (context) => context[0].label,
-        label: (context) => {
-          const value = context.parsed.x;
-          const percentage = ((value / locationTotal) * 100).toFixed(1);
-          return `Amount: ${value} (${percentage}%)`;
-        },
-        footer: () => `Total: ${locationTotal}`,
-      },
-    },
-    legend: {
-      display: false,
-    },
-    datalabels: {
-      font: {
-        weight: 'bold',
-      },
-    },
-  },
-};
-
-
-
-// Filter out the data entry with an empty "_id"
-const filteredAnswersData = data.answersData.filter((item) => item._id !== '');
-
-// Calculate the total count of all answers
-const totalAnswers = filteredAnswersData.reduce((sum, item) => sum + item.count, 0);
-
-// Prepare the data for the answers chart
-const answersLabels = filteredAnswersData.map((item) => item._id);
-const answersData = filteredAnswersData.map((item) => item.count);
-
-const answersChartData = {
-  labels: answersLabels,
-  datasets: [
-    {
-      label: 'Count',
-      data: answersData,
-      backgroundColor: 'rgba(153, 102, 255, 0.5)',
       datalabels: {
-        color: 'black',
-        anchor: 'end',
-        align: 'top',
-        formatter: (value, context) => {
-          const percentage = ((value / totalAnswers) * 100).toFixed(1);
-          return `${percentage}%`;
+        font: {
+          weight: "bold",
         },
       },
     },
-  ],
-};
+  };
 
-const answersChartOptions = {
-  responsive: true,
-  plugins: {
-    tooltip: {
-      callbacks: {
-        label: (context) => {
-          const value = context.parsed.y;
-          const percentage = ((value / totalAnswers) * 100).toFixed(1);
-          return `Count: ${value} (${percentage}%)`;
+  // Filter out the data entry with an empty "_id"
+  const filteredAnswersData = data.answersData.filter(
+    (item) => item._id !== ""
+  );
+
+  // Calculate the total count of all answers
+  const totalAnswers = filteredAnswersData.reduce(
+    (sum, item) => sum + item.count,
+    0
+  );
+
+  // Prepare the data for the answers chart
+  const answersLabels = filteredAnswersData.map((item) => item._id);
+  const answersData = filteredAnswersData.map((item) => item.count);
+
+  const answersChartData = {
+    labels: answersLabels,
+    datasets: [
+      {
+        label: "Count",
+        data: answersData,
+        backgroundColor: "rgba(153, 102, 255, 0.5)",
+        datalabels: {
+          color: "black",
+          anchor: "end",
+          align: "top",
+          formatter: (value, context) => {
+            const percentage = ((value / totalAnswers) * 100).toFixed(1);
+            return `${percentage}%`;
+          },
+        },
+      },
+    ],
+  };
+
+  const answersChartOptions = {
+    responsive: true,
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            const value = context.parsed.y;
+            const percentage = ((value / totalAnswers) * 100).toFixed(1);
+            return `Count: ${value} (${percentage}%)`;
+          },
+        },
+      },
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        font: {
+          weight: "bold",
         },
       },
     },
-    legend: {
-      display: false,
-    },
-    datalabels: {
-      font: {
-        weight: 'bold',
-      },
-    },
-  },
-};
+  };
 
-// Register the datalabels plugin
-ChartJS.register(ChartDataLabels);
+  // Register the datalabels plugin
+  ChartJS.register(ChartDataLabels);
 
   return (
     <div>
-      <h1>Survey Data Visualization</h1>
-
       <div>
-        <h2>Age and Gender </h2>
+        <h2 >Age and Gender </h2>
+
         <div style={{ display: "flex" }}>
           <div style={{ flex: 2 }}>
             <Bar data={ageGenderChartData} options={ageGenderChartOptions} />
           </div>
-
-          <div>
-            <div style={{ display: "flex" }}>
+          <div
+            style={{
+              flex: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <div>
               <div
                 style={{
-                  flex: 2,
                   display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "flex-start",
-                  marginLeft: "20px",
+                  alignItems: "center",
+                  marginBottom: "10px",
                 }}
               >
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <div
-                    style={{
-                      width: "20px",
-                      height: "20px",
-                      backgroundColor: "#e3f4ee",
-                      marginRight: "10px",
-                    }}
-                  ></div>
-                  <div>
-                    Male:{" "}
-                    {data.ageGenderData.reduce(
-                      (sum, item) => sum + item.male,
-                      0
-                    )}
-                  </div>
+                <div
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    backgroundColor: "#e3f4ee",
+                    marginRight: "10px",
+                  }}
+                ></div>
+                <div>
+                  Male:{" "}
+                  {data.ageGenderData.reduce((sum, item) => sum + item.male, 0)}
                 </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <div
-                    style={{
-                      width: "20px",
-                      height: "20px",
-                      backgroundColor: "#44f1b6",
-                      marginRight: "10px",
-                    }}
-                  ></div>
-                  <div>
-                    Female:{" "}
-                    {data.ageGenderData.reduce(
-                      (sum, item) => sum + item.female,
-                      0
-                    )}
-                  </div>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginBottom: "10px",
+                }}
+              >
+                <div
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    backgroundColor: "#44f1b6",
+                    marginRight: "10px",
+                  }}
+                ></div>
+                <div>
+                  Female:{" "}
+                  {data.ageGenderData.reduce(
+                    (sum, item) => sum + item.female,
+                    0
+                  )}
                 </div>
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <div
-                    style={{
-                      width: "20px",
-                      height: "20px",
-                      backgroundColor: "#44f1b6",
-                      marginRight: "10px",
-                    }}
-                  ></div>
-                  <div>Other: 0</div>
-                </div>
+              </div>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <div
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    backgroundColor: "#44f1b6",
+                    marginRight: "10px",
+                  }}
+                ></div>
+                <div>Other: 0</div>
               </div>
             </div>
           </div>
